b4f66e05bf37cf6a1ecc36fdea62b37233adfa48
kochdara <darakoch57@gmail.com>
2025-07-24 08:47:32 +0700

    updated
    updated

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { useHead } from '@vueuse/head'
+
+useHead({
+  title: 'My Awesome Article',
+  meta: [
+    { name: 'description', content: 'This is a powerful Vue 3 article for SEO.' },
+    
+    // Facebook / Open Graph
+    { property: 'og:title', content: 'My Awesome Article' },
+    { property: 'og:description', content: 'Learn how to add Facebook SEO support in Vue 3.' },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: 'https://demo-vue-sharing.netlify.app' },
+    { property: 'og:image', content: 'https://demo-vue-sharing.netlify.app/content.png' },
+    { property: 'og:image:width', content: '1024' },
+    { property: 'og:image:height', content: '1024' },
+
+    // Twitter Card
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:title', content: 'My Awesome Article' },
+    { name: 'twitter:description', content: 'Learn how to add Twitter SEO support in Vue 3.' },
+    { name: 'twitter:image', content: 'https://demo-vue-sharing.netlify.app/content.png' }
+  ]
+})
 </script>
 
 <template>
-  <RouterView />
+  <h1>My Awesome Article</h1>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,11 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from './App.vue'
 import { createHead } from '@vueuse/head'
 
-import App from './App.vue'
-import router from './router'
-
 const app = createApp(App)
-
 const head = createHead()
+
 app.use(head)
-
-app.use(createPinia())
-app.use(router)
-
 app.mount('#app')
